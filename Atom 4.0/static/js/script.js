bd49231b41c7e236837c19167d1bed29f8fa5f9a
Tr1nside <petya.08.tomsk@gmail.com>
2025-03-02 23:10:15 +0700

    Автокоммит: 2025-03-02 23:10:15

--- Atom 4.0/static/js/script.js
+++ Atom 4.0/static/js/script.js
@@ -20,8 +20,6 @@ addfile2.onclick = function() {
 
 delfile2.onclick = function() {
     file2.hidden = true;
-    file2.hidden = true;
-    file2.hidden = true;
     count -= 1
     if (count == 1){
         addfile2.hidden = false;
