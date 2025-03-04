49a79ad6c5670eac0443032454a9c15ceb543136
Tr1nside <petya.08.tomsk@gmail.com>
2025-03-04 13:37:12 +0700

    Автокоммит: 2025-03-04 13:37:12

--- Atom 3.1/run.py
+++ Atom 3.1/run.py
@@ -2,7 +2,7 @@ from app.__init__ import socketio, app
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='127.0.0.1', port=5000)
+    socketio.run(app, debug=True, host='127.0.0.1', port=5001)
 
 
 # all for run server
