2e05e56398539b08034a24b759ebeeaa8b083fc0
Tr1nside <petya.08.tomsk@gmail.com>
2025-03-04 20:36:14 +0700

    Автокоммит: 2025-03-04 20:36:14

--- Atom/static/js/script.js
+++ Atom/static/js/script.js
@@ -5,6 +5,14 @@ let codeMirrorInstances = {};
 const consoleOutput = document.querySelector('.console-output');
 const consoleInput = document.querySelector('.console-input');
 const socket = io();
+const pythonKeywords = [
+    "and", "as", "assert", "break", "class", "continue", "def", "del",
+    "elif", "else", "except", "False", "finally", "for", "from", "global",
+    "if", "import", "in", "is", "lambda", "None", "nonlocal", "not", "or",
+    "pass", "raise", "return", "True", "try", "while", "with", "yield"
+];
+
+
 
 consoleInput.addEventListener('focus', () => {
     if (consoleInput.readOnly) {
