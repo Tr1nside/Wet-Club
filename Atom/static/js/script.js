7dc3131f46e1932b06c59ac8d1171b12374813f3
Tr1nside <petya.08.tomsk@gmail.com>
2025-03-04 21:37:16 +0700

    Автокоммит: 2025-03-04 21:37:15

--- Atom/static/js/script.js
+++ Atom/static/js/script.js
@@ -271,42 +271,12 @@ tabs.addEventListener('dblclick', (event) => {
 // Инициализация начальной вкладки и CodeMirror
 const initialTab = document.querySelector('.tab[data-tab="tab1"]');
 const initialCodeArea = document.querySelector('.code-area[data-tab-content="tab1"]');
-// 🔹 Инициализируем CodeMirror
-const cm = CodeMirror(document.querySelector(".code-area"), {
+const cm = CodeMirror(initialCodeArea, {
     mode: "python",
     theme: body.classList.contains('dark-mode') ? "dracula" : "default",
     lineNumbers: true,
-    gutters: ["CodeMirror-linenumbers"],
-    
-    // 🔹 Автозакрытие скобок, кавычек, квадратных и фигурных скобок
-    autoCloseBrackets: true,
-
-    // 🔹 Подключаем "Ctrl-Space" для автодополнения
-    extraKeys: {
-        "Ctrl-Space": "autocomplete",
-        "Tab": function(cm) {
-            if (cm.somethingSelected()) {
-                cm.indentSelection("add");
-            } else {
-                cm.replaceSelection("    ", "end", "+input");
-            }
-        },
-        "Shift-Tab": function(cm) {
-            cm.indentSelection("subtract");
-        },
-        "Ctrl-/": function(cm) {
-            cm.execCommand("toggleComment");
-        }
-    }
+    gutters: ["CodeMirror-linenumbers"]
 });
-
-// 🔹 Автодополнение при вводе текста
-cm.on("inputRead", function(cm, change) {
-    if (change.text[0].match(/\w/)) { // Если введён символ (буква/цифра)
-        cm.showHint({ hint: pythonHint, completeSingle: false });
-    }
-});
-
 codeMirrorInstances['tab1'] = cm;
 activateTab(initialTab);
 initialTab.addEventListener('dblclick', function () {
