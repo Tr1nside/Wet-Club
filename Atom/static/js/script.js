d3387da0927cb8440ecdc86187445af1ce799d54
Tr1nside <petya.08.tomsk@gmail.com>
2025-03-04 21:50:04 +0700

    Автокоммит: 2025-03-04 21:50:04

--- Atom/static/js/script.js
+++ Atom/static/js/script.js
@@ -8,20 +8,28 @@ const socket = io();
 
 // 🔹 Список ключевых слов и встроенных функций Python для автодополнения
 const pythonKeywords = [
+    // Ключевые слова Python
     "False", "None", "True", "and", "as", "assert", "async", "await",
     "break", "class", "continue", "def", "del", "elif", "else", "except",
     "finally", "for", "from", "global", "if", "import", "in", "is",
     "lambda", "nonlocal", "not", "or", "pass", "raise", "return", "try",
-    "while", "with", "yield", "abs", "all", "any", "bin", "bool",
-    "bytearray", "bytes", "callable", "chr", "classmethod", "compile",
-    "complex", "delattr", "dict", "dir", "divmod", "enumerate", "eval",
-    "exec", "filter", "float", "format", "frozenset", "getattr", "globals",
-    "hasattr", "hash", "help", "hex", "id", "input", "int", "isinstance",
-    "issubclass", "iter", "len", "list", "locals", "map", "max", "memoryview",
-    "min", "next", "object", "oct", "open", "ord", "pow", "print", "property",
-    "range", "repr", "reversed", "round", "set", "setattr", "slice", "sorted",
-    "staticmethod", "str", "sum", "super", "tuple", "type", "vars", "zip"
-];
+    "while", "with", "yield",
+  
+    // Встроенные функции Python
+    "abs", "all", "any", "bin", "bool", "bytearray", "bytes", "callable",
+    "chr", "classmethod", "compile", "complex", "delattr", "dict", "dir",
+    "divmod", "enumerate", "eval", "exec", "filter", "float", "format",
+    "frozenset", "getattr", "globals", "hasattr", "hash", "help", "hex",
+    "id", "input", "int", "isinstance", "issubclass", "iter", "len",
+    "list", "locals", "map", "max", "memoryview", "min", "next", "object",
+    "oct", "open", "ord", "pow", "print", "property", "range", "repr",
+    "reversed", "round", "set", "setattr", "slice", "sorted", "staticmethod",
+    "str", "sum", "super", "tuple", "type", "vars", "zip",
+  
+    // Дополнительно часто используемые идентификаторы
+    "self", "init", "main", "os", "sys", "json", "time", "re", "math",
+    "random", "datetime", "open", "read", "write", "close"
+  ];
 
 // 🔹 Функция для автодополнения
 function pythonHint(cm) {
