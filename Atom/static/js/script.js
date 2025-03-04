077c5e7efd40d4ccbbfdc9e57b60aad24a2de7ad
Tr1nside <petya.08.tomsk@gmail.com>
2025-03-04 20:44:33 +0700

    Автокоммит: 2025-03-04 20:44:33

--- Atom/static/js/script.js
+++ Atom/static/js/script.js
@@ -6,10 +6,37 @@ const consoleOutput = document.querySelector('.console-output');
 const consoleInput = document.querySelector('.console-input');
 const socket = io();
 const pythonKeywords = [
-    "and", "as", "assert", "break", "class", "continue", "def", "del",
-    "elif", "else", "except", "False", "finally", "for", "from", "global",
-    "if", "import", "in", "is", "lambda", "None", "nonlocal", "not", "or",
-    "pass", "raise", "return", "True", "try", "while", "with", "yield"
+    // Ключевые слова Python
+    "False", "None", "True", "and", "as", "assert", "async", "await",
+    "break", "class", "continue", "def", "del", "elif", "else", "except",
+    "finally", "for", "from", "global", "if", "import", "in", "is",
+    "lambda", "nonlocal", "not", "or", "pass", "raise", "return", "try",
+    "while", "with", "yield",
+
+    // Встроенные функции Python
+    "abs", "all", "any", "ascii", "bin", "bool", "bytearray", "bytes",
+    "callable", "chr", "classmethod", "compile", "complex", "delattr",
+    "dict", "dir", "divmod", "enumerate", "eval", "exec", "filter", "float",
+    "format", "frozenset", "getattr", "globals", "hasattr", "hash", "help",
+    "hex", "id", "input", "int", "isinstance", "issubclass", "iter", "len",
+    "list", "locals", "map", "max", "memoryview", "min", "next", "object",
+    "oct", "open", "ord", "pow", "print", "property", "range", "repr",
+    "reversed", "round", "set", "setattr", "slice", "sorted", "staticmethod",
+    "str", "sum", "super", "tuple", "type", "vars", "zip",
+
+    // Встроенные типы
+    "int", "float", "bool", "str", "list", "tuple", "set", "dict", "complex",
+
+    // Часто используемые стандартные модули и функции
+    "os", "sys", "re", "math", "random", "datetime", "time", "json", "subprocess",
+    "logging", "itertools", "functools", "collections", "threading", "multiprocessing",
+    "pathlib", "shutil", "argparse", "socket", "http", "urllib", "email", "sqlite3",
+    "decimal", "fractions", "statistics", "heapq", "bisect", "copy", "pprint",
+
+    // Дополнительные часто используемые идентификаторы
+    "self", "cls", "init", "main", "data", "value", "result", "error", "file",
+    "read", "write", "open", "close", "config", "args", "kwargs", "key", "item",
+    "index", "count", "sum", "avg", "mean", "min", "max"
 ];
 
 
