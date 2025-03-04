eee5f6aaf79e1127789bc824548e08b245fd6357
Xodichiy Protein <kulesh.pa@gmail.com>
2025-03-04 15:00:41 +0700

    Автокоммит: 2025-03-04 15:00:41

--- users/Pavel/website/app/routes.py
+++ users/Pavel/website/app/routes.py
@@ -5,15 +5,4 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return 'hello world'
