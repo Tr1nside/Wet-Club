c92ebdf3b6d1f6e4720a9b7dfd5e6b4372e8600d
Tr1nside <petya.08.tomsk@gmail.com>
2025-03-05 12:58:42 +0700

    Автокоммит: 2025-03-05 12:58:42

--- AiLab/app/__init__.py
+++ AiLab/app/__init__.py
@@ -7,18 +7,13 @@ from config import Config
 import os
 
 MAIN_FOLDER = os.path.dirname(os.path.abspath(__file__))
-def create_app():
-    app = Flask(__name__)
 
-    app = Flask(__name__,
+app = Flask(__name__,
             static_folder=os.path.join(MAIN_FOLDER, '..', 'static'),
             template_folder=os.path.join(MAIN_FOLDER, '..', 'templates')) # Создаём Flask-приложение
-    app.config.from_object(Config)
+app.config.from_object(Config)
 
-    socketio = SocketIO(app) # Инициализируем SocketIO
-    app.register_blueprint(main_bp) # Регистрируем Blueprint
-    register_socketio_events(socketio) # Регистрируем события для SocketIO (обработчики on('execute'), on('console_input'), и т.д.)
-
-
-    return app
+socketio = SocketIO(app) # Инициализируем SocketIO
+app.register_blueprint(main_bp) # Регистрируем Blueprint
+register_socketio_events(socketio) # Регистрируем события для SocketIO (обработчики on('execute'), on('console_input'), и т.д.)
 
