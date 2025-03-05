59c6fdb0735cdbbe84621709287e4b36e96eef20
Tr1nside <petya.08.tomsk@gmail.com>
2025-03-05 15:39:38 +0700

    Автокоммит: 2025-03-05 15:39:38

--- AiLab/app/routes.py
+++ AiLab/app/routes.py
@@ -76,5 +76,5 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect('/main')
     return render_template('login.html', title='Sign In', form=form)
