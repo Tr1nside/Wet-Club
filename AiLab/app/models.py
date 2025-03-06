236042bd1bc4eba5f5c3068ce3a2c6d2d59fae0c
Tr1nside <petya.08.tomsk@gmail.com>
2025-03-06 21:29:13 +0700

    Автокоммит: 2025-03-06 21:29:13

--- AiLab/app/models.py
+++ AiLab/app/models.py
@@ -1,12 +1,7 @@
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import app
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+from app import db
 
 class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
